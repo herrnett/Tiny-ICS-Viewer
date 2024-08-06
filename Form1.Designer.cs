02c0538524dc79de5e6f4c62f5d2dc2cc9ca34fd
herrnett <tim@herrnett.de>
2024-08-06 13:39:11 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ICS_Viewer_C_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DTStart = new System.Windows.Forms.RichTextBox();
+            this.DTEnd = new System.Windows.Forms.RichTextBox();
+            this.Location = new System.Windows.Forms.RichTextBox();
+            this.Summary = new System.Windows.Forms.RichTextBox();
+            this.Description = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DTStart
+            // 
+            this.DTStart.Location = new System.Drawing.Point(12, 12);
+            this.DTStart.Name = "DTStart";
+            this.DTStart.Size = new System.Drawing.Size(510, 32);
+            this.DTStart.TabIndex = 0;
+            this.DTStart.Text = "";
+            // 
+            // DTEnd
+            // 
+            this.DTEnd.Location = new System.Drawing.Point(12, 50);
+            this.DTEnd.Name = "DTEnd";
+            this.DTEnd.Size = new System.Drawing.Size(510, 32);
+            this.DTEnd.TabIndex = 1;
+            this.DTEnd.Text = "";
+            // 
+            // Location
+            // 
+            this.Location.Location = new System.Drawing.Point(12, 88);
+            this.Location.Name = "Location";
+            this.Location.Size = new System.Drawing.Size(510, 32);
+            this.Location.TabIndex = 2;
+            this.Location.Text = "";
+            // 
+            // Summary
+            // 
+            this.Summary.Location = new System.Drawing.Point(12, 126);
+            this.Summary.Name = "Summary";
+            this.Summary.Size = new System.Drawing.Size(510, 32);
+            this.Summary.TabIndex = 3;
+            this.Summary.Text = "";
+            // 
+            // Description
+            // 
+            this.Description.Location = new System.Drawing.Point(12, 164);
+            this.Description.Name = "Description";
+            this.Description.Size = new System.Drawing.Size(510, 232);
+            this.Description.TabIndex = 4;
+            this.Description.Text = "";
+            this.Description.TextChanged += new System.EventHandler(this.Description_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 399);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(510, 53);
+            this.label1.TabIndex = 5;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Description);
+            this.Controls.Add(this.Summary);
+            this.Controls.Add(this.Location);
+            this.Controls.Add(this.DTEnd);
+            this.Controls.Add(this.DTStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tiny ICS Viewer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox DTStart;
+        private System.Windows.Forms.RichTextBox DTEnd;
+        private System.Windows.Forms.RichTextBox Location;
+        private System.Windows.Forms.RichTextBox Summary;
+        private System.Windows.Forms.RichTextBox Description;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ICS_Viewer_C_
+{
+    public partial class Form1 : Form
+    {
+
+        string[] texts = { "Begins", "Ends", "Location", "Title", "Description" };
+        public Form1()
+        {
+
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+            DTStart.AllowDrop = true;
+            DTStart.DragEnter += new DragEventHandler(Form1_DragEnter);
+            DTStart.DragDrop += new DragEventHandler(Form1_DragDrop);
+            DTStart.Text = texts[0];
+            DTEnd.AllowDrop = true;
+            DTEnd.DragEnter += new DragEventHandler(Form1_DragEnter);
+            DTEnd.DragDrop += new DragEventHandler(Form1_DragDrop);
+            DTEnd.Text = texts[1];
+            Location.AllowDrop = true;
+            Location.DragEnter += new DragEventHandler(Form1_DragEnter);
+            Location.DragDrop += new DragEventHandler(Form1_DragDrop);
+            Location.Text = texts[2];
+            Summary.AllowDrop = true;
+            Summary.DragEnter += new DragEventHandler(Form1_DragEnter);
+            Summary.DragDrop += new DragEventHandler(Form1_DragDrop);
+            Summary.Text = texts[3];
+            Description.AllowDrop = true;
+            Description.DragEnter += new DragEventHandler(Form1_DragEnter);
+            Description.DragDrop += new DragEventHandler(Form1_DragDrop);
+            Description.Text = texts[4];
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            DTStart.Text = texts[0];
+            DTEnd.Text = texts[1];
+            Location.Text = texts[2];
+            Summary.Text = texts[3];
+            Description.Text = texts[4];
+
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if ( files != null && files.Length > 0 )
+            {
+                string readContents;
+                using (StreamReader streamReader = new StreamReader(files[0], Encoding.UTF8))
+                {
+                    readContents = streamReader.ReadToEnd();
+                    string[] seperators = { "VEVENT","VALARM" };
+                    string[] parts = readContents.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+                    parts = parts[1].Split('\n');
+
+                    string[] temp;
+                    string year;
+                    string month;
+                    string day;
+                    string hour;
+                    string min;
+                    foreach (string part in parts)
+                    {
+                        if (part.StartsWith("LOCATION"))
+                        {
+                            temp = part.Split(':');
+                            Location.Text = "Location: " + temp[1];
+                        }
+                        if (part.StartsWith("SUMMARY"))
+                        {
+                            temp = part.Split(':');
+                            Summary.Text = "Title: " + temp[1];
+                        }
+                        if (part.StartsWith("DESCRIPTION"))
+                        {
+                            seperators = new string[] { "DESCRIPTION:" };
+                            temp = part.Split(seperators, StringSplitOptions.None);
+                            Description.Text = "Description: " + temp[1];
+                        }
+                        if (part.StartsWith("DTSTART"))
+                        {
+                            temp = part.Split(':');
+                            year = temp[1].Substring(0, 4);
+                            month = temp[1].Substring(4, 2);
+                            day = temp[1].Substring(6, 2);
+                            hour = temp[1].Substring(9, 2);
+                            min = temp[1].Substring(11, 2);
+                            temp = temp[0].Split('=');
+                            DTStart.Text = "Begins: " + day + "." + month + "." + year + " um " + hour + ":" + min + ". Time zone: " + temp[1];
+                        }
+                        if (part.StartsWith("DTEND"))
+                        {
+                            temp = part.Split(':');
+                            year = temp[1].Substring(0, 4);
+                            month = temp[1].Substring(4, 2);
+                            day = temp[1].Substring(6, 2);
+                            hour = temp[1].Substring(9, 2);
+                            min = temp[1].Substring(11, 2);
+                            temp = temp[0].Split('=');
+                            DTEnd.Text = "Ends: " + day + "." + month + "." + year + " um " + hour + ":" + min + ". Time Zone: " + temp[1];
+                        }
+                    }   
+                }
+            }
+        }
+
+        private void Description_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

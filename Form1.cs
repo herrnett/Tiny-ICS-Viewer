a29d28a7919d8bb8e7c0abe5f4c288ff711957f0
herrnett <tim@herrnett.de>
2024-08-08 00:52:59 +0200

    fixed license

--- Form1.cs
+++ Form1.cs
@@ -69,7 +69,7 @@ namespace ICS_Viewer_C_
             //Test if the "About..." item was selected from the system menu
             if ((m.Msg == WM_SYSCOMMAND) && ((int)m.WParam == SYSMENU_ABOUT_ID))
             {
-                MessageBox.Show("iCal.NET (https://github.com/rianjs/ical.net)\r\n\r\nThis software includes the iCal.NET Library: Copyright (C) 2016 Douglas Day, Rian Stockbower\r\n\r\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\r\n\r\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.");
+                MessageBox.Show("This software includes the iCal.NET library (https://github.com/rianjs/ical.net)\r\n\r\nCopyright (C) 2016 Douglas Day, Rian Stockbower\r\n\r\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\r\n\r\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.");
             }
 
         }

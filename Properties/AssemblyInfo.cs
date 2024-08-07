2544bec2ee84d514ff802a153ddb0c370edde906
herrnett <tim@herrnett.de>
2024-08-08 01:06:46 +0200

    Assembly info update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tiny ICS Viewer")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © 2024 herrnett")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
